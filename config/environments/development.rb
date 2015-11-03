baec455910798f457e996e145ee966c67c158d88
Sharpgames <sharpgames@users.noreply.github.com>
2015-11-03 18:45:51 +0900

    Update development.rb
    Not to show Bullet.alert

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,12 +36,12 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-	  config.after_initialize do
-			    Bullet.enable = true # Bulletプラグインを有効
-					    Bullet.alert = true # JavaScriptでの通知
-							    Bullet.bullet_logger = true # log/bullet.logへの出力
-									    Bullet.console = true # ブラウザのコンソールログに記録
-											    Bullet.rails_logger = true # Railsログに出力
-													  end
+  config.after_initialize do
+	Bullet.enable = true # Bulletプラグインを有効
+	#Bullet.alert = true # JavaScriptでの通知
+	Bullet.bullet_logger = true # log/bullet.logへの出力
+	Bullet.console = true # ブラウザのコンソールログに記録
+	Bullet.rails_logger = true # Railsログに出力
+  end
 
 end
